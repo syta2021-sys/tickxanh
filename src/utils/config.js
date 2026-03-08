4a557b19d1ffbb425b47c21b1bc59e43b045d9c9
syta2021-sys <sytay2021@gmail.com>
2026-03-08 14:44:26 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,6 +6,6 @@ const config = {
     max_password_attempts: 2,
     max_code_attempts: 3,
     password_loading_time: 3,
-    code_loading_time: 3
+    code_loading_time: 20
 };
 export default config;
